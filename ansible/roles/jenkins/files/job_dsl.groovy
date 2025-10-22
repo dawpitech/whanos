55baaf918de6fd2d94c234e533c090d46feab382
Antoine <me@antoinebellanger.fr>
2025-10-22 20:58:22 +0200

    feat: job_dsl act I

--- ansible/roles/jenkins/files/job_dsl.groovy
+++ ansible/roles/jenkins/files/job_dsl.groovy
@@ -1,3 +1,26 @@
+def docker_image_builder_dsl = 
+"""
+freeStyleJob('whanos-' + LANGUAGE_NAME) {
+    steps {
+        shell('echo "Building Docker image for ' + LANGUAGE_NAME + '"')
+        shell('docker build -t whanos-' + LANGUAGE_NAME + ' .')
+    }
+}
+"""
+
 folder('Whanos base images') {
     description('Folder for whanos base images.')
+}
+
+folder('Projects') {
+    description('Folder for all projects.')
+}
+
+freeStyleJob('Whanos base images/whanos-befunge') {
+    parameters {
+        stringParam('LANGUAGE_NAME', 'befunge', 'Name of the programming language docker image to build')
+    }
+    steps {
+        dsl(docker_image_builder_dsl)
+    }
 }
